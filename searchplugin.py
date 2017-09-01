be182360a7a29a461970fa442ef0ad9ec68e1194
Jens Domke <domke.j.aa@m.titech.ac.jp>
2017-09-01 16:24:01 +0900

    fixing import errors with Python and Gtk+ 3

--- searchplugin.py
+++ searchplugin.py
@@ -5,11 +5,19 @@ See copyright file that comes with this software for full licence
 
 Modified by cgw 2011/11/06
 Modified by aramboi 2016/08/16
+Modified by jdomke 2017/09/01
 """
-import gtk
-import urllib
-import terminatorlib.plugin as plugin
-import re
+from urllib import quote
+from sys import exit
+try:
+    from gi import require_version
+    require_version('Gtk', '3.0')
+    from gi.repository import Gtk, Gdk
+except ImportError:
+    print('You need Gtk 3.0+ to run this SearchPlugin.')
+    exit(1)
+from terminatorlib.plugin import MenuItem
+from re import compile
 
 try:
     from searchplugin_config import *
@@ -20,10 +28,10 @@ except ImportError:
 # AVAILABLE must contain a list of all the classes that you want exposed
 AVAILABLE = ['SearchPlugin']
 
-_spaces = re.compile(" +")
+_spaces = compile(" +")
 
 
-class SearchPlugin(plugin.Plugin):
+class SearchPlugin(MenuItem):
     capabilities = ['terminal_menu']
 
     def do_search(self, searchMenu):
@@ -32,15 +40,15 @@ class SearchPlugin(plugin.Plugin):
             return
         uri = SEARCH_ENGINE_URI.format(
             urllib.quote_plus(self.searchstring.encode("utf-8")))
-        gtk.show_uri(None, uri, gtk.gdk.CURRENT_TIME)
+        Gtk.show_uri(None, uri, Gdk.CURRENT_TIME)
 
     def callback(self, menuitems, menu, terminal):
         """Add our menu item to the menu"""
         self.terminal = terminal
-        item = gtk.ImageMenuItem(gtk.STOCK_FIND)
+        item = Gtk.ImageMenuItem(Gtk.STOCK_FIND)
         item.connect('activate', self.do_search)
         if terminal.vte.get_has_selection():
-            clip = gtk.clipboard_get(gtk.gdk.SELECTION_PRIMARY)
+            clip = Gtk.Clipboard.get(Gdk.SELECTION_PRIMARY)
             self.searchstring = clip.wait_for_text().strip()
             self.searchstring = self.searchstring.replace("\n", " ")
             self.searchstring = self.searchstring.replace("\t", " ")
@@ -48,8 +56,8 @@ class SearchPlugin(plugin.Plugin):
         else:
             self.searchstring = None
         if self.searchstring:
-            if len(self.searchstring) > 40:
-                displaystring = self.searchstring[:37] + "..."
+            if len(self.searchstring) > 16:
+                displaystring = self.searchstring[:13] + "..."
             else:
                 displaystring = self.searchstring
             item.set_label(
